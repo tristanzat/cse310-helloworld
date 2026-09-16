2f2ad7f3251ea6cb67cfa24c2574b3da00dd0590
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-09-16 10:32:55 -0600

    add hello world program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace helloworld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
